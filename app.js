a3194ff23a326e4f38caaa1dcf91c2c561469415
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-27 09:31:16 +0530

    updated

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Pratiksha:3K24A5lbaC6mP3ZR@cluster0-2g3xi.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri,{ useUnifiedTopology: true }, { useNewUrlParser: true });
 client.connect(err => {
- const col = client.db("Quiz").collection("Question");
+ const col = client.db("Quiz").collection("Questions");
  console.log("connect");
 //  let ins={16 : 'Who has been appointed to Board of Dierctions in Paytm Payments Bank (PPB)?'};
- var dil = col.findOne({"test_id":1});
-    //  if(err) throw err;
-   console.log(dil);
+col.find({"q1":1}, function(err, result) {
+   if (err) throw err;
+   console.log(result.question);
+   client.close();
+ });
  
 //  let show = collection.findOne();
 //  collection.findOne({}, function(err, result) {
