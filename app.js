3cae5ea7eccc801099adb63c890e456bd12b347a
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-27 09:51:57 +0530

    updated

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-
+const express =require('express');
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Pratiksha:3K24A5lbaC6mP3ZR@cluster0-2g3xi.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri,{ useUnifiedTopology: true }, { useNewUrlParser: true });
+const app = express();
+const port = 8100;
 client.connect(err => {
- const col = client.db("Quiz").collection("Questions");
+const col = client.db("Quiz").collection('Questions');
  console.log("connect");
-//  let ins={16 : 'Who has been appointed to Board of Dierctions in Paytm Payments Bank (PPB)?'};
-col.find({"q1":1}, function(err, result) {
-   if (err) throw err;
-   console.log(result.question);
-   client.close();
- });
- 
-//  let show = collection.findOne();
-//  collection.findOne({}, function(err, result) {
-//     if (err) throw err;
-//     console.log(result.name);
-//     db.close();
-//   });
-//   console.log(show);
-// perform actions on the collection object
+ if (err)
+ return console.log(err);
+require('./app/routes')(app, col);
+app.listen(port, () => {
+ console.log("Port 8100 running on browser...");
+});
  client.close();
 });
+
+
